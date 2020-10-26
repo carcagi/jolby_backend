b937d8533aed928d136d226d4079623290c514d8
Marisol2201 <1529@holbertonschool.com>
2020-10-26 09:22:19 -0500

    reported offers structure

--- functions/index.js
+++ functions/index.js
@@ -31,5 +31,6 @@ app.get("/api/v1/refresh", (req, res) => {
 // Routes
 app.use(require("./routes/jobs_routes"));
 app.use(require("./routes/users_routes"));
+app.use(require("./routes/companies_routes.js"));
 
 exports.app = functions.https.onRequest(app);
